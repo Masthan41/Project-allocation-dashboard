183a7013ad2d5165ed8a7aa878c12fe2acd8e905
Abdul Harish Khan <khan.abdulharish@outlook.com>
2025-08-24 16:17:11 +0530

    7 commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    console.log("🔎 MONGODB_URI:", process.env.MONGODB_URI); // <-- debug
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    console.log("🔎 MONGODB_URI:", process.env.MONGO_URI); // <-- debug
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
